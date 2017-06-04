3f760425f90b4fcff9172ca4fbb3b900f2a981d9
Marius Wiggert <marius.wiggert@cdtm.de>
2017-06-04 23:10:56 +0200

    New feature that enabls multiple lanes simultaneously

--- LED/LED.ino
+++ LED/LED.ino
@@ -1,18 +1,21 @@
 #include <Adafruit_NeoPixel.h>
 #include "Arduino.h"
 
-int LED_STRIP_PIN = 6;
+// Lane 1
+const int LED_STRIP_PIN_LANE_1 = 5;
+const int BUTTON_PIN_SHOOT_LANE_1 = 10;
+const int BUTTON_PIN_DEFEND_LANE_1 = 11;
+// Lane 2
+const int LED_STRIP_PIN_LANE_2 = 6;
+const int BUTTON_PIN_SHOOT_LANE_2 = 11;
+const int BUTTON_PIN_DEFEND_LANE_2 = 10;
 
-int BUTTON_PIN_P1 = 10;
-int BUTTON_PIN_P2 = 13;
+// Number of LEDs of a single strip
+const int PIXELS_LANE_1 = 50; 
+const int PIXELS_LANE_2 = 50;
 
 const byte IMPULS_MIN_SIZE = 4; // Minimum length of an impulse
-
-const int PIXELS = 150; // Number of LEDs of a single strip
-
 int Lifes = 100;
-
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIXELS, LED_STRIP_PIN, NEO_GRB);
 
 struct RGB {
   byte r;
@@ -20,6 +23,8 @@ struct RGB {
   byte b;
 };
 
+// Colors structs
+RGB red;
 
 class Impuls {
   public:
@@ -35,21 +40,47 @@ Impuls::Impuls(int pos, byte len, RGB col) {
   color = col;
 };
 
+class Lane {
+  public:
+    Adafruit_NeoPixel* strip;
+    //dynamically allocated array of *Impuls
+    Impuls** impulses;
+    int current_index;
+    Lane(int num_pixels, int led_pin, int button1, int button2);
+    void createNewImpuls();
+    void loop();
+    
+  private:
+    int button1;
+    int button2;
+    int size_impulses_array;
+};
 
-// Colors
-RGB red;
+//Lane Constructor
+Lane::Lane(int num_pixels, int led_pin, int but1, int but2){
+  strip= new Adafruit_NeoPixel(num_pixels, led_pin, NEO_GRB);
+  strip->begin();
+  strip->show(); // Initialize all LEDs to be off
+  strip->setBrightness(150);
+  // Array of impulses
+  // size_impulses is the number of concurrent impulses per strip. The idea behind the calculation is to
+  // have a number where we would have no overlapping impulses in position and length that can fit across
+  // a single strip
+  size_impulses_array = num_pixels / (IMPULS_MIN_SIZE + 1); 
+//  impulses=(Impuls**)malloc(size_impulses_array*sizeof(Impuls*));
+  impulses = new Impuls*[size_impulses_array];
+  // Fill Array with DummyImpulses
+  for (int i = 0; i < size_impulses_array; i++) {
+    impulses[i] = new Impuls(-1, IMPULS_MIN_SIZE, red);
+  }
+  current_index=-1;
+  button1=but1;
+  button2=but2;
+};
 
-// Array of impulses
-//
-// size_impulses is the number of concurrent impulses per strip. The idea behind the calculation is to
-// have a number where we would have no overlapping impulses in position and length that can fit across
-// a single strip
-const int size_impulses = PIXELS / (IMPULS_MIN_SIZE + 1); 
-Impuls *impulses[size_impulses];
-int current_index = -1;
-
-void createNewImpuls() {
-  current_index = (current_index + 1) % size_impulses; // Rolling index, overwriting old impulses
+//New Impuls Function declaration
+void Lane::createNewImpuls() {
+  current_index = (current_index + 1) % size_impulses_array; // Rolling index, overwriting old impulses
 
   // Re-initializè the impulse at the current position
   impulses[current_index]->position = 0;
@@ -57,40 +88,15 @@ void createNewImpuls() {
   impulses[current_index]->color = red;
 };
 
-void setup() { // Running once after Arduino boots
-  strip.begin();
-  
-  strip.show(); // Initialize all LEDs to be off
-  strip.setBrightness(150);
-  
-  Serial.begin(9600); // TODO: Remove if we only use Serial for print statements
-
-  // Initialize "red" to be red
-  red.r = 255;
-  red.g = 0;
-  red.b = 0;
-
-  Serial.println(size_impulses);
-
-  // Fill Array with DummyImpulses
-  for (int i = 0; i < size_impulses; i++) {
-    impulses[i] = new Impuls(-1, IMPULS_MIN_SIZE, red);
-  }
-}
-
-void loop() { // Should run once every ~33ms to receive 30 frames per second
-   // Record the current milliseconds to track how long the logic
-   // takes to execute
-  int ms= millis();
-
+void Lane::loop(){
   // Check whether the first player is pressing her button
-  if (digitalRead(BUTTON_PIN_P1) == HIGH) {
+  if (digitalRead(button1) == HIGH) {
     // Create a new impulse if there has never been an impulse created (current_index == -1)
     // or if the last impulse is no longer at the beginning of the strip. (position > 1)
     // TODO: Impulses can currently overlap,
     // we should see whether this can become a problem at some point
-    if (
-      current_index == -1 ||
+    Serial.println("Shoot Button is HIGH");
+    if (current_index == -1 ||
       impulses[current_index]->position > 1
     ) {
       createNewImpuls();
@@ -98,19 +104,21 @@ void loop() { // Should run once every ~33ms to receive 30 frames per second
     else {
       // We should extend the current impulse, because it is still at the beginning.
       // We reset the position and extend its length to give the impression of a
-      // longer impulse.      
+      // longer impulse.
       impulses[current_index]->position = 0;
       impulses[current_index]->length++;
     }
+  } else {
+      Serial.println("Shoot Button is LOW");
   }
 
   // Turn all pixels off
-  for (int i = 0; i < PIXELS; i++) {
-    strip.setPixelColor(i, 0, 0, 0);
+  for (int i = 0; i < strip->numPixels(); i++) {
+    strip->setPixelColor(i, 0, 0, 0);
   }
   
   // Loop over all impulses
-  for (int i = 0; i < size_impulses; i++) {
+  for (int i = 0; i < size_impulses_array; i++) {
     // Check whether the impulse is valid and initialized, otherwise skip this impulse
     if (impulses[i]->position == -1) {
       continue;
@@ -118,22 +126,22 @@ void loop() { // Should run once every ~33ms to receive 30 frames per second
 
     // Colorize all relevant pixels of the strip
     int pos = impulses[i]->position;
-    for (int j = 0; j < impulses[i]->length && pos + j < PIXELS; j++) {
-      strip.setPixelColor(pos + j, impulses[i]->color.r, impulses[i]->color.g, impulses[i]->color.b);
+    for (int j = 0; j < impulses[i]->length && pos + j < strip->numPixels(); j++) {
+      strip->setPixelColor(pos + j, impulses[i]->color.r, impulses[i]->color.g, impulses[i]->color.b);
     }
 
     // Check whether the impulse is at the other end of the strip. If it is at the other end
     // and the second player is not pressing her button reduce the lifes of this player.
     if (
-      pos + impulses[i]->length - 2 >= PIXELS - 1 &&
-      digitalRead(BUTTON_PIN_P2)==LOW
+      pos + impulses[i]->length - 2 >= strip->numPixels() - 1 &&
+      digitalRead(button2)==LOW
     ) {
       Lifes--;
     }
 
     // If the impulse is at the end of the strip, "disable" it,
     // by setting the position back to the invalid value of -1
-    if (pos == PIXELS - 1) {
+    if (pos == strip->numPixels() - 1) {
       impulses[i]->position = -1;
     }
     else {
@@ -143,11 +151,39 @@ void loop() { // Should run once every ~33ms to receive 30 frames per second
   }
 
   // Show all pixels
-  strip.show();
+  strip->show();
+};
 
+const int NUM_LANES = 2;
+Lane* Lanes_Array[NUM_LANES];
+
+void setup() { // Running once after Arduino boots
+  Serial.begin(9600); // TODO: Remove if we only use Serial for print statements
+
+  // Initialize "red" to be red
+  red.r = 255;
+  red.g = 0;
+  red.b = 0;
+
+  Lanes_Array[0]=new Lane(PIXELS_LANE_1, LED_STRIP_PIN_LANE_1, BUTTON_PIN_SHOOT_LANE_1, BUTTON_PIN_DEFEND_LANE_1);
+  Lanes_Array[1]=new Lane(PIXELS_LANE_2, LED_STRIP_PIN_LANE_2, BUTTON_PIN_SHOOT_LANE_2, BUTTON_PIN_DEFEND_LANE_2);
+}
+
+void loop() { 
+   // Should run once every ~33ms to receive 30 frames per second
+   // Record the current milliseconds to track how long the logic
+   // takes to execute
+  int ms= millis();
+  for (int i=0; i<NUM_LANES; i++){
+      Lanes_Array[i]->loop();
+    }
+
+  
   // Calculate the time it took to execute this iteration.
   ms = millis() - ms;
-
+  int delayTime = 17 - ms;
   // Wait at least 33 milliseconds until we continue with the next iteration
-  delay(17 - ms);
+  if (delayTime > 0) {
+    delay(delayTime);
+  }
 }
