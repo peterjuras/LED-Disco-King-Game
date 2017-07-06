2333f2d861b2782be0a88b989b54dcadd065290e
Peter Juras <me@peterjuras.com>
2017-07-06 10:44:14 +0200

    Add sounds

---
+++ Arduino-Raspberry-Connection/Arduino-Raspberry-Connection.ino
@@ -0,0 +1,8 @@
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  Serial.println(25);
+  delay(1000);
+}
---
+++ Arduino-Raspberry-Connection/raspberry_server.py
@@ -0,0 +1,45 @@
+import time
+import serial
+import pygame.mixer
+
+SER = serial.Serial('/dev/ttyACM0', 9600)
+
+NUM_CHANNELS = 32
+pygame.mixer.init()
+pygame.mixer.set_num_channels(NUM_CHANNELS)
+print pygame.mixer.get_num_channels()
+
+MUSIC = {
+    '$M0': '/home/pi/src/LED-Disco-King-Game/Arduino-Raspberry-Connection/Sounds/Music/Stayin-Alive.mp3'
+}
+
+SOUNDS = {
+    '$S0': '/home/pi/src/LED-Disco-King-Game/Arduino-Raspberry-Connection/Sounds/UT/doublekill.wav',
+    '$S1': '/home/pi/src/LED-Disco-King-Game/Arduino-Raspberry-Connection/Sounds/UT/assasin.wav'
+}
+
+def play_music(serial_input):
+    pygame.mixer.music.stop()
+    pygame.mixer.music.load(MUSIC[serial_input])
+    pygame.mixer.music.play()
+
+current_channel = 0
+
+def play_sound(serial_input):
+    global current_channel
+    pygame.mixer.Channel(current_channel).play(pygame.mixer.Sound(SOUNDS[serial_input]))
+    current_channel = (current_channel + 1) % NUM_CHANNELS
+
+def play(serial_input):
+    if serial_input.startswith('$M'):
+        play_music(serial_input)
+    elif serial_input.startswith('$S'):
+        play_sound(serial_input)
+
+play('$M0')
+
+while True:
+    SERIAL_INPUT = serial.readline()
+
+    print 'Playing sound'
+    play(SERIAL_INPUT)
